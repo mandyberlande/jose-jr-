d04b0b67b2aed6162b0d64d91020a826929c36cd
mandyberlande <amanda.berlande.marcelino@escola.pr.gov.br>
2025-10-02 11:12:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function tocarSom(id) {
-    const audios = document.querySelectorAll('audio');
-    audios.forEach(audio => {
-      audio.pause();
-      audio.currentTime = 0;
-    });
-    
-    const audioSelecionado = document.getElementById(id);
-    audioSelecionado.play();
-  }
+	const audios = document.querySelectorAll('audio');
+	audios.forEach(audio => {
+		audio.pause();
+		audio.currentTime = 0;
+	});
+	const audioSelecionado = document.querySelector(id);
+	if (audioSelecionado) {
+		audioSelecionado.play();
+	}
+}
